26c3cd8d00f426055d0a5eb17ed30aeccdf803f9
PRISCILLA URIBE <puribe0@student.ubc.ca>
2023-08-16 15:23:37 -0400

    second user story: add and show new restaurants

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -3,4 +3,32 @@ class RestaurantsController < ApplicationController
   def index
     @restaurants = Restaurant.all
   end
+  # User story 2: A visitor can add a new restaurant, and be redirected to the show view of that new restaurant.
+  # GET "restaurants/new" and POST "restaurants"
+  def new
+    @restaurant = Restaurant.new
+  end
+
+  def show
+  end
+
+  def create
+    # @restaurant = Restaurant.new(restaurants_params)
+
+    if @restaurant.save
+      redirect_to @restaurant, notice: "Your restaurant was created."
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def set_restaurant
+    @restaurant = Restaurant.find(params[:restaurant_id])
+  end
+
+  def review_params
+    params.require(:review).permit(:content)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  resources :restaurants, only: [:index]
+  resources :restaurants, only: [:index, :new, :create, :show]
   end
 
 #   # Defines the root path route ("/")
